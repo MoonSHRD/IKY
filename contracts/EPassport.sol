dc02f793a455dfc9fb5e3b63dde84cf9dbc95574
Bekket <jackatwork@yandex.ru>
2022-05-25 17:02:15 +0300

    e-passport boilerplate

---
+++ contracts/EPassport.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+// TODO: documentation
+
+// This contract implements (https://www.facebook.com/100000943480007/posts/pfbid0fZFB2g6wHMfZ2Bu432hZjZJEpmMU1gaYyJAhs7HGfadyhDYPAfNN4KWoPv45pdQSl/?d=n)
+// e-Passport is a contract which allow to create electronic passport of citizens with external KYC check of documents
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EPassport is Ownable {
+
+
+
+
+}
