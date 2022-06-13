3626afb913f269a8f9ffe7d60803146602cac994
Bekket <jackatwork@yandex.ru>
2022-06-13 18:19:45 +0300

    MetaMask Authorization

---
+++ webapp/webapp/src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+import { Layout } from 'components/layout'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <ChakraProvider>
+        <Layout>
+        <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+  )
+}
+
+export default MyApp
