c85666146f8c35499bc6bb1a3179a60c556f86c0
Bekket <jackatwork@yandex.ru>
2022-06-14 19:50:24 +0300

    read component boilerplate

--- webapp/webapp/src/components/read_contract.tsx
+++ webapp/webapp/src/components/read_contract.tsx
@@ -22,6 +22,7 @@ export default function ReadERC20(props:Props){
   const currentAccount = props.currentAccount
   const [totalSupply,setTotalSupply]=useState<string>()
   const [symbol,setSymbol]= useState<string>("")
+  const [owner, setOwner] = useState<string>()
 
 
   // called only once, use it as constructor
@@ -30,21 +31,32 @@ export default function ReadERC20(props:Props){
 
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const TGPassport = new ethers.Contract(addressContract, abi, provider);
-    erc20.symbol().then((result:string)=>{
-        setSymbol(result)
-    }).catch('error', console.error)
+    TGPassport.owner().then((result:string)=>{
+        setOwner(result)
+    }).catch('error', console.error);
 
+    /*
     erc20.totalSupply().then((result:string)=>{
         setTotalSupply(ethers.utils.formatEther(result))
     }).catch('error', console.error);
+    */
     //called only once
   },[])  
 
   return (
+    /**
     <div>
-        <Text><b>ERC20 Contract</b>: {addressContract}</Text>
+        <Text><b>Telegram Passport Contract</b>: {addressContract}</Text>
         <Text><b>ClassToken totalSupply</b>:{totalSupply} {symbol}</Text>
         <Text my={4}><b>ClassToken in current account</b>:</Text>
     </div>
+    */
+
+    <div>
+        <Text><b>Telegram Passport Contract</b>: {addressContract}</Text>
+        <Text><b>Owner of contract</b>:{owner}</Text>
+        <Text my={4}><b>ClassToken in current account</b>:</Text>
+    </div>
+
   )
 }
