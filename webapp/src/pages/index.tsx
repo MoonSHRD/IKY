8155ba82e8e8476b32a3ec2d141751d8237122c2
Bekket <jackatwork@yandex.ru>
2022-09-12 19:00:43 +0300

    updated addresses rinkeby

--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -95,21 +95,21 @@ const Home: NextPage = () => {
           <Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
           <Heading my={4}  fontSize='xl'>Read ClassToken Info</Heading>
           <ReadPassportContract 
-            addressContract='0x1361463b0F4B46f71E0CA41b6cEf3B72bd4B4c20'
+            addressContract='0x5BC6Ec8e805a0A0f1dC287C873D67a99e31cA690'
             currentAccount={currentAccount}
           />
         </Box>
         <Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
           <Heading my={4}  fontSize='xl'>Attach telegram id to your wallet</Heading>
           <ApplyPassportTG 
-            addressContract='0x1361463b0F4B46f71E0CA41b6cEf3B72bd4B4c20'
+            addressContract='0x5BC6Ec8e805a0A0f1dC287C873D67a99e31cA690'
             currentAccount={currentAccount}
           />
         </Box>
         <Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
           <Heading my={4}  fontSize='xl'>Approve user intent for registration personal wallet address</Heading>
           <ApprovePassportTG 
-            addressContract='0x1361463b0F4B46f71E0CA41b6cEf3B72bd4B4c20'
+            addressContract='0x5BC6Ec8e805a0A0f1dC287C873D67a99e31cA690'
             currentAccount={currentAccount}
           />
         </Box>
