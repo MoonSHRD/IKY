deb224f418f747872a7fbbd08c91f9b4c62554ed
Bekket <jackatwork@yandex.ru>
2022-09-14 22:05:52 +0300

    post-build path fix

--- contracts/Union.sol
+++ contracts/Union.sol
@@ -10,21 +10,21 @@ pragma solidity ^0.8.0;
 //import "hardhat/console.sol";
 
 // direct imports -- use it for compile contracts and webapp
-/*
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-*/
+
 
 // relative imports (for building ABI and go) -- use it for build
 
-
+/*
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-
+*/
 
 
 import "./TGPassport.sol";
