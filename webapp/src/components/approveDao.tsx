ac518ea6602fc3f1327ee0e4612bfb9fc159ba67
Bekket <jackatwork@yandex.ru>
2022-09-14 21:58:14 +0300

    prototyped dao page and components

--- webapp/src/components/approveDao.tsx
+++ webapp/src/components/approveDao.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import {Button, Input , NumberInput,  NumberInputField,  FormControl,  FormLabel } from '@chakra-ui/react'
 import {ethers} from 'ethers'
 import {parseEther } from 'ethers/lib/utils'
-import {abi} from '../../../artifacts/contracts/TGPassport.sol/TGPassport.json'
+import {abi} from '../../../artifacts/contracts/Union.sol/Union.json'
 import { Contract } from "ethers"
 import { TransactionResponse,TransactionReceipt } from "@ethersproject/abstract-provider"
 import {useRouter} from "next/router";
@@ -14,37 +14,37 @@ interface Props {
 
 declare let window: any;
 
-export default function DeclinePassportTG(props:Props){
+export default function ApproveDaoTG(props:Props){
   const addressContract = props.addressContract
   const currentAccount = props.currentAccount
-  const [declined_user_wallet, setDeclinedUserWallet] = useState<string>("")
+  const [approved_dao_wallet, setApprovedDaoWallet] = useState<string>("")
 
   const { query } = useRouter();
 
 
   // function for bot, for approving passport. make it here for tests
-  async function declinePassport(event:React.FormEvent) { 
+  async function approveDao(event:React.FormEvent) { 
     event.preventDefault()
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
-    const TGPassport:Contract = new ethers.Contract(addressContract, abi, signer)
+    const Union:Contract = new ethers.Contract(addressContract, abi, signer)
 
-    TGPassport.DeclinePassport(declined_user_wallet)
+    Union.ApproveJoin(approved_dao_wallet)
      .then((tr: TransactionResponse) => {
         console.log(`TransactionResponse TX hash: ${tr.hash}`)
-        tr.wait().then((receipt:TransactionReceipt) => {console.log("decline receipt", receipt)})
+        tr.wait().then((receipt:TransactionReceipt) => {console.log("approving receipt", receipt)})
         })
        .catch((e:Error) => console.log(e))
   }
 
-  const handleChange = (value:string) => setDeclinedUserWallet(value)
+  const handleChange = (value:string) => setApprovedDaoWallet(value)
 
   return (
-    <form onSubmit={declinePassport}>
+    <form onSubmit={approveDao}>
     <FormControl>
       <FormLabel htmlFor='WADDR'>Wallet Address: </FormLabel>
-      <Input id="declined_user_wallet" type="text" required  onChange={(e) => setDeclinedUserWallet(e.target.value)} value={query.user_wallet} my={3}/>
-      <Button type="submit" isDisabled={!currentAccount}>Decline Passport</Button>
+      <Input id="approved_user_wallet" type="text" required  onChange={(e) => setApprovedDaoWallet(e.target.value)} value={query.approved_dao_wallet} my={3}/>
+      <Button type="submit" isDisabled={!currentAccount}>Approve DAO</Button>
     </FormControl>
     </form>
   )
