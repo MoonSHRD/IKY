c1e060b6f6e9cde921ef74f020373cc648d04116
Bekket <jackatwork@yandex.ru>
2022-11-17 21:11:03 +0400

    go files

--- contracts/Union.sol
+++ contracts/Union.sol
@@ -26,6 +26,7 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
 */
 
 
